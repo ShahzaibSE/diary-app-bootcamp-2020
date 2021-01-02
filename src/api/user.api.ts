d1aa99af27cebb4714fb010147660411651a8fa9
Shahzaib <shahzaibnoorcs@gmail.com>
2021-01-02 22:43:25 +0500

    User API updated

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -7,4 +7,18 @@ export const login = (user: any) => {
     let login_requestBody = user as Partial<User>
     console.log("Login credientials")
     console.log(login_requestBody) 
+    http.post("/auth/signin",login_requestBody).then((result:any)=> {
+        console.log("Login Response")
+        console.log(result)
+    })
+}
+
+export const signup = (user: any) => {
+    let login_requestBody = user as Partial<User>
+    console.log("Signup user details")
+    console.log(login_requestBody) 
+    http.post("/auth/create",login_requestBody).then((result:any)=> {
+        console.log("Sign up response")
+        console.log(result)
+    })
 }
