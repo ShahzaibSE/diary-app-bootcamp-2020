5eaf602ccf3054afe462cb86705299a07823c841
Shahzaib <shahzaibnoorcs@gmail.com>
2021-01-13 20:18:55 +0500

    Diary slice updated

--- src/features/diary/diary.slice.ts
+++ src/features/diary/diary.slice.ts
@@ -1,7 +1,17 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+// Model.
+import {Diary} from "./../../models/diary.interface";
 
 export const diarySlice = createSlice({
     name:"diary",
-    initialState:{},
-    reducers:{}
+    initialState:[] as Diary[],
+    reducers:{
+        addDiary(state:any, {payload}:PayloadAction<Diary[]>){
+            const diariestosave = payload.filter((diary:any) => {
+                return state.findIndex((item: any)=>(item.id == diary.id)) === -1
+            })
+            state.push(...diariestosave)
+        }
+    }
 })
+
