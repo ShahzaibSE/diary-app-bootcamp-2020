9fb3a7c6e669f6c69edab8eb67d66b50fbdcee9d
Shahzaib <shahzaibnoorcs@gmail.com>
2021-01-14 22:42:53 +0500

    Combined reducers.

---
+++ src/app_store/rootReducer.ts
@@ -0,0 +1,19 @@
+import {combineReducers} from "@reduxjs/toolkit";
+// Reducers.
+import authReducer from "./../features/auth/auth.slice";
+import userReducer from "./../features/auth/user.slice";
+import diaryReducer from "./../features/diary/diary.slice";
+import entryReducer from "./../features/entry/entry.slice";
+import editorReducer from "./../features/entry/editor.slice";
+
+export const rootReducer = combineReducers({
+    auth: authReducer,
+    user: userReducer,
+    diary: diaryReducer,
+    entry: entryReducer,
+    editor: editorReducer
+})
+
+export type RootState = ReturnType<typeof rootReducer>;
+export default rootReducer;
+ 
--- src/app_store/store.ts
+++ src/app_store/store.ts
@@ -1,13 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit";
 // Reducers.
-import authReducer from "./../features/auth/auth.slice";
-import userReducer from "./../features/auth/user.slice";
-import diaryReducer from "./../features/diary/diary.slice";
+import rootReducer from "./rootReducer";
 
 export default configureStore({
-    reducer: {
-        authReducer,
-        userReducer,
-        diaryReducer
-    }
+    reducer: rootReducer
 })
