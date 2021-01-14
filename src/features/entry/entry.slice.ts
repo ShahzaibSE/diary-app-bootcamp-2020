67f7d9f3d7634d55392fe22363ed343fcff1c481
Shahzaib <shahzaibnoorcs@gmail.com>
2021-01-14 17:45:52 +0500

    Entry slice added and updated.

--- src/features/entry/entry.slice.ts
+++ src/features/entry/entry.slice.ts
@@ -1,7 +1,23 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+// Model.
+import {Entry} from "./../../models/entry.interface";
 
 export const entrySlice = createSlice({
     name:"entry",
-    initialState:{},
-    reducers:{}
+    initialState:[] as Entry[],
+    reducers:{
+        setEntry(state:any, {payload}:PayloadAction<Entry[]|null>){
+            return state = payload != null ? payload : []
+        },
+        updateEntry(state:any, {payload}:PayloadAction<Entry>){
+            const {id} = payload
+            const index = state.findIndex((entry:any)=>(id === entry.id))
+            if (index !== -1){
+                state.splice(index, 1, payload)
+            }
+        }
+    }
 })
+
+export const {setEntry, updateEntry} = entrySlice.actions
+export default entrySlice.reducer
