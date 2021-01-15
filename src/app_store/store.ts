61387c81daa84e7502a041969828bba5cadead48
Shahzaib <shahzaibnoorcs@gmail.com>
2021-01-15 15:34:17 +0500

    store updated

--- src/app_store/store.ts
+++ src/app_store/store.ts
@@ -1,7 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {useDispatch} from "react-redux";
 // Reducers.
 import rootReducer from "./rootReducer";
 
-export default configureStore({
+export const store = configureStore({
     reducer: rootReducer
 })
+
+type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export default store
+
