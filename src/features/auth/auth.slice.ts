a665ff501cf4344e00b7303649806fc8d1fc056b
Shahzaib <shahzaibnoorcs@gmail.com>
2021-01-20 23:37:43 +0500

    Auth slice updated

--- src/features/auth/auth.slice.ts
+++ src/features/auth/auth.slice.ts
@@ -1,5 +1,4 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import { Action } from "history";
 
 interface AuthState {
     token: string | null
@@ -15,16 +14,16 @@ export const authSlice = createSlice({
     name:"auth",
     initialState,
     reducers:{
-        saveToken(state:any, action: any)  {
-            if(action.payload !== null){
-                state.token = action.payload
+        saveToken(state:any, {payload}: PayloadAction<any>)  {
+            if(payload !== null){
+                state.token = payload
             }
         },
         clearToken(state:any){
             state.token = null
         },
-        setAuthState(state:any, action: any){
-            state.isAuthenticated = action.payload
+        setAuthState(state:any, {payload}: PayloadAction<any>){
+            state.isAuthenticated = payload
         }
     }
 })
