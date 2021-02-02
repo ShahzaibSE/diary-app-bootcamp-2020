029002aba67dfe162e27fbaca7e17be05583f249
Shahzaib <shahzaibnoorcs@gmail.com>
2021-02-02 18:19:14 +0500

    Updated

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,9 +1,13 @@
 import React, {FC, useState, useEffect} from 'react';
 import {RootState} from "./../../app_store/rootReducer";
-import Markdown from 'markdown-to-jsx';
-import {http} from "./../../api/index.api";
+import {useSelector} from "react-redux";
+// Models.
 import {Diary} from "./../../models/diary.interface";
 import {Entry} from "./../../models/entry.interface";
+// API.
+import {http} from "./../../api/index.api";
+// Features.
+import {setCurrentlyEditing, setCanEdit} from "./../../features/entry/editor.slice";
 
 
 const Editor:FC = () => {
