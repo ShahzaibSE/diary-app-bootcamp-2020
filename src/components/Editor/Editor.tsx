f520f15f644d3f731a3e31999c823e2b01ab0f7f
Shahzaib <shahzaibnoorcs@gmail.com>
2021-02-04 11:10:07 +0500

    Saving and editing entry.

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -8,7 +8,7 @@ import {Entry} from "./../../models/entry.interface";
 import {http} from "./../../api/index.api";
 // Features.
 import {setCurrentlyEditing, setCanEdit} from "./../../features/entry/editor.slice";
-import {updateDiary} from "./../../features/diary/diary.slice";
+import {diarySlice, updateDiary} from "./../../features/diary/diary.slice";
 import {updateEntry} from "./../../features/entry/entry.slice";
 import {showAlert} from "./../../mock_server/utils";
 import {useAppDispatch} from "./../../app_store/store";
@@ -18,6 +18,30 @@ const Editor:FC = () => {
     const { currentlyEditing: entry, canEdit, activeDiaryId } = useSelector((state: RootState) => state.editor)
     const [editedEntry, updateEditedEntry] = useState(entry)
     const dispatch = useAppDispatch()
+    // 
+    const saveEntry = () => {
+        if (activeDiaryId == null) {
+            showAlert("Please select a diary", "warning")
+        }else if(entry == null) {
+            http.post<Entry, {diary: Diary, entry: Entry}>(`/diaries/entry/${activeDiaryId}`, editedEntry)
+                .then((data: any) => {
+                    if (data != null) {
+                        const {diary, entry: _entry} = data
+                        dispatch(setCurrentlyEditing(_entry))
+                        dispatch(updateDiary(diary))
+                    }
+                })
+        }else {
+            http.put<Entry, {diary: Diary, entry: Entry}>(`/diaries/entry/${entry.id}`,editedEntry)
+                .then((_entry: any)=>{
+                    if(_entry != null) {
+                        dispatch(setCurrentlyEditing(_entry))
+                        dispatch(updateEntry(_entry))
+                    }
+                })
+        }
+        dispatch(setCanEdit(false))
+    }
     return (
         <div>
             
