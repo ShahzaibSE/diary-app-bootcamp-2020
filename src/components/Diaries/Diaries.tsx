dcc0c045b5d060054f316f785fff2685e6b076b3
Shahzaib <shahzaibnoorcs@gmail.com>
2021-04-13 19:26:57 +0500

    Updated

--- src/components/Diaries/Diaries.tsx
+++ src/components/Diaries/Diaries.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import {Outlet} from "react-router-dom";
 
 const Diaries = () => {
     return (
         <div>
-            
+            <Outlet/>
         </div>
     )
 }
